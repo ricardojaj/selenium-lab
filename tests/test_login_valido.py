9d0fe21fd076af2b19b9dbea1fe9b92ac05c722c
Ricardo Anjos <83664969+ricardojaj@users.noreply.github.com>
2024-08-24 15:29:54 -0300

    Update test_login_valido.py

--- tests/test_login_valido.py
+++ tests/test_login_valido.py
@@ -9,9 +9,9 @@ from pages.login_page import LoginPage
 
 class Test_Login:
     def test_login_valido(self, setup_teardown):
-        browser = conftest.browser;
-        login_page = LoginPage();
+        browser = conftest.browser
+        login_page = LoginPage()
         home_page = HomePage()
 
-        login_page.fazer_login("standard_user", "secret_sauce");
+        login_page.fazer_login("standard_user", "secret_sauce")
         home_page.verificar_login_com_sucesso()
