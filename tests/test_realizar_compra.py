be056985fca0291b0ff817b48f5ad32a2d18c5f4
Ricardo Anjos <83664969+ricardojaj@users.noreply.github.com>
2024-08-24 15:30:28 -0300

    Update test_realizar_compra.py

--- tests/test_realizar_compra.py
+++ tests/test_realizar_compra.py
@@ -11,20 +11,20 @@ from pages.login_page import LoginPage
 @pytest.mark.usefixtures("setup_teardown")
 class Test_realizar_compra:
     def test_finalizando_compra(self, setup_teardown):
-        browser = conftest.browser;
-        login_page = LoginPage();
-        fechar_pedido = FecharPedido();
+        browser = conftest.browser
+        login_page = LoginPage()
+        fechar_pedido = FecharPedido()
 
-        login_page.fazer_login("standard_user", "secret_sauce");
+        login_page.fazer_login("standard_user", "secret_sauce")
 
-        carrinho_page = CarrinhoPage();
-        carrinho_page.adcProdutosCarrinho();
+        carrinho_page = CarrinhoPage()
+        carrinho_page.adcProdutosCarrinho()
 
         carrinho_page.acessoCarrinho()
         carrinho_page.adcNovoProduto()
 
         #Click checkout button
-        fechar_pedido.acessarTelaChecout();
+        fechar_pedido.acessarTelaChecout()
 
         #Preenchendo informaçoes
         fechar_pedido.formularioDeCompra("Mariana", "Santos", "11111111")
