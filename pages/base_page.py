10d333769d75af47fd0601b67e5f9ecbb3de2991
Ricardo Anjos <83664969+ricardojaj@users.noreply.github.com>
2024-08-24 15:33:20 -0300

    Update base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,22 +3,22 @@ import conftest
 
 class BasePage:
     def __init__(self):
-        self.driver = conftest.browser;
+        self.driver = conftest.browser
 
     def encontrar_elemento(self, locator):
-        return self.driver.find_element(*locator);
+        return self.driver.find_element(*locator)
 
     def encontrar_elementos(self, locator):
-        return self.driver.find_elements(*locator);
+        return self.driver.find_elements(*locator)
 
     def escrever(self, locator, text):
-        self.encontrar_elemento(locator).send_keys(text);
+        self.encontrar_elemento(locator).send_keys(text)
 
     def clicar(self, locator):
-        self.encontrar_elemento(locator).click();
+        self.encontrar_elemento(locator).click()
 
     def verificar_se_elemento_existe(self, locator):
-        assert self.encontrar_elemento(locator).is_displayed(), f"O elemento '{locator}' nao foi encontrado na tela";
+        assert self.encontrar_elemento(locator).is_displayed(), f"O elemento '{locator}' nao foi encontrado na tela"
 
     def verificar_elemento_nao_existe(self, locator):
         assert len(self.encontrar_elementos(locator)) == 0, f"O elemento '{locator}' nao existe"
