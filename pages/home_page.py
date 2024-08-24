ed6f622407004e1e0c1bc781bbc4ce13caf99f0d
Ricardo Anjos <83664969+ricardojaj@users.noreply.github.com>
2024-08-24 15:31:57 -0300

    Update home_page.py

--- pages/home_page.py
+++ pages/home_page.py
@@ -7,14 +7,14 @@ from pages.base_page import BasePage
 class HomePage(BasePage):
 
     def __init__(self):
-        self.driver = conftest.browser;
-        self.titulo_pagina = (By.XPATH, "//span[@class='title']");
+        self.driver = conftest.browser
+        self.titulo_pagina = (By.XPATH, "//span[@class='title']")
         self.mensagem_login_invalido = (By.XPATH, "//h3[@data-test='error']")
 
     def verificar_login_com_sucesso(self):
-        self.verificar_se_elemento_existe(self.titulo_pagina);
+        self.verificar_se_elemento_existe(self.titulo_pagina)
 
     def verificar_login_invalido(self):
-        self.verificar_se_elemento_existe(self.mensagem_login_invalido);
+        self.verificar_se_elemento_existe(self.mensagem_login_invalido)
 
 
