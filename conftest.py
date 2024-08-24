a32294b110349924ea10a39285e2e06f08fcdc23
Ricardo Anjos <83664969+ricardojaj@users.noreply.github.com>
2024-08-24 15:31:10 -0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,16 +1,16 @@
 import pytest
 from selenium import webdriver
 
-browser: webdriver.Remote;
+browser: webdriver.Remote
 
 @pytest.fixture
 def setup_teardown(scope="session"):
     # setup
     global browser
-    browser = webdriver.Chrome();
+    browser = webdriver.Chrome()
     browser.implicitly_wait(5)
-    browser.get("https://www.saucedemo.com/");
-    browser.maximize_window();
+    browser.get("https://www.saucedemo.com/")
+    browser.maximize_window()
 
     #run test
     yield browser
