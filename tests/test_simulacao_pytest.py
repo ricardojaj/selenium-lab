56f270a350851fc268f63773401285af5bf0d21d
Ricardo Anjos <83664969+ricardojaj@users.noreply.github.com>
2024-08-24 15:30:47 -0300

    Update test_simulacao_pytest.py

--- tests/test_simulacao_pytest.py
+++ tests/test_simulacao_pytest.py
@@ -3,7 +3,7 @@ import pytest
 @pytest.mark.simulacao
 class TestSimulacao():
     def test_simulacao_1(self):
-        assert 1 == 1;
+        assert 1 == 1
 
     def test_simulacao_2(self):
-        assert 1 == 1;
+        assert 1 == 1
